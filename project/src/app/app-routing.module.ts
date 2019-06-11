b032d37edf2f7b78fe867c475c58b8f7de0b2964
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2019-06-11 05:44:35 -0300

    Criando componentes e rotas das telas de items e lists

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoriesComponent } from './categories/categories.component';
+import { ListsComponent } from './lists/lists.component';
+import { ItemsComponent } from './items/items.component';
 
 const routes: Routes = [
-  { path: '', component: CategoriesComponent }
+  { path: '', redirectTo: 'categories', pathMatch: 'full' },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: 'items', component: ItemsComponent }
 ];
 
 @NgModule({
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { ListsComponent } from './lists/lists.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    ListsComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
